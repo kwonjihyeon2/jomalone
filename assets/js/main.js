2d66d3fa630f5d0e40bbbfde78752cd0c00c5ab1
kwonjihyeon2 <kjh2868@gmail.com>
2021-11-08 18:17:36 +0900

    gift 고민

--- assets/js/main.js
+++ assets/js/main.js
@@ -37,16 +37,31 @@ let scents = document.getElementById("scents");
 let pear = document.getElementById("pear");
 let freesia = document.getElementById("freesia");
 
+// window.addEventListener('scroll',function(){
+//     var value = -600+window.pageYOffset;
+
+//     scents.style.right = -value * 0.5 + 'px' ;
+//     pear.style.top = value * 0.5 + 'px' ;
+// });
+
+// window.addEventListener('scroll',function(){
+//     var value = -600+window.pageYOffset;
+
+//     freesia.style.right = value * 0.5 + 'px' ;
+// })
+
 window.addEventListener('scroll',function(){
-    var value = -800+window.pageYOffset;
+    var value = -750+window.pageYOffset;
 
-    scents.style.right = -value * 0.3 + 'px' ;
-    pear.style.top = value * 0.3 + 'px' ;
-});
-
-window.addEventListener('scroll',function(){
-    var value = -850+window.pageYOffset;
-
-    freesia.style.right = value * 0.3 + 'px' ;
+    if( window.pageYOffset<800){
+        scents.style.right = -value * 0.5 + 'px' ;
+        pear.style.top = value * 0.5 + 'px' ;
+        freesia.style.right = value * 0.5 + 'px' ;
+    }
+    else {
+        scents.style.right= "-35px";
+        freesia.style.right= "35px";
+        pear.style.top='35px';
+    }
 })
 
